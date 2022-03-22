864a216d6ac43bf212b3df5e83d376eee08e0740
Scott <scottlangmead@hotmail.co.uk>
2022-03-22 16:24:41 +0000

    Basic SQL query from world.sql

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -1,21 +1,32 @@
+/**
+ * This is the primary entry point of the program.
+ * Application extracts and processes data from a MySQL database.
+ * Created by: Scott Langmead, Matthew Wilson, Md Saifullah and Rowen Peebles (Group 11)
+ * Created at: Edinburgh Napier University (Software Engineering Methods Module)
+ **/
+
 package com.napier.sem;
 
 import java.sql.*;
+import java.util.ArrayList;
 
 public class App
 {
     public static void main(String[] args)
-        {
-            // Create new Application
-            App a = new App();
+    {
+        // Create new Application
+        App a = new App();
 
-            // Connect to database
-            a.connect();
+        // Connect to database
+        a.connect();
 
-            // Disconnect from database
-            a.disconnect();
-        }
+        // Basic function to test database connection
+        City city = a.getCity(3);
+        a.displayCity(city);
 
+        // Disconnect from database
+        a.disconnect();
+    }
 
     /**
      * Connection to MySQL database.
@@ -47,7 +58,7 @@ public class App
                 // Wait a bit for db to start
                 Thread.sleep(30000);
                 // Connect to database
-                con = DriverManager.getConnection("jdbc:mysql://db:3306/employees?useSSL=false", "root", "example");
+                con = DriverManager.getConnection("jdbc:mysql://db:3306/world?useSSL=false", "root", "example");
                 System.out.println("Successfully connected");
                 break;
             }
@@ -82,4 +93,48 @@ public class App
         }
     }
 
+    public City getCity(int ID)
+    {
+        try
+        {
+            // Create an SQL statement
+            Statement stmt = con.createStatement();
+            // Create string for SQL statement
+            String strSelect =
+                    "SELECT ID, Name, Population "
+                            + "FROM city "
+                            + "WHERE ID = " + ID;
+            // Execute SQL statement
+            ResultSet rset = stmt.executeQuery(strSelect);
+            // Return new employee if valid.
+            // Check one is returned
+            if (rset.next())
+            {
+                City city = new City();
+                city.id = rset.getInt("ID");
+                city.name = rset.getString("Name");
+                city.population = rset.getInt("Population");
+                return city;
+            }
+            else
+                return null;
+        }
+        catch (Exception e)
+        {
+            System.out.println(e.getMessage());
+            System.out.println("Failed to get employee details");
+            return null;
+        }
+    }
+
+    public void displayCity(City city)
+    {
+        if (city != null)
+        {
+            System.out.println(
+                city.id + " "
+                    + city.name + " "
+                    + "Population:" + city.population + "\n");
+        }
+    }
 }
---
+++ src/main/java/com/napier/sem/City.java
@@ -0,0 +1,33 @@
+package com.napier.sem;
+
+/**
+ * Represents a City. Class representation of the table created in world.sql
+ * @author Pablo Sanchez
+ */
+
+public class City {
+    /**
+     * ID of the city
+     */
+    public int id;
+
+    /**
+     * Name of the City
+     */
+    public String name;
+
+    /**
+     * Country Code of the City
+     */
+    public String countryCode;
+
+    /**
+     * District of the city
+     */
+    public String district;
+
+    /**
+     * Population of the City
+     */
+    public int population;
+}
